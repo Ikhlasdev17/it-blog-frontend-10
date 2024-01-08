b31b4b325d6417f85cd8bbd00cfa4a3e88106174
Ikhlasdeveloper77 <xeneje8700@apdiv.com>
2024-01-08 18:33:32 +0500

    configuration completed

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import type { Metadata } from 'next'
+import { Montserrat } from 'next/font/google'
+import './global.scss'
+import './globals.scss'
+
+const montserrat = Montserrat({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+	title: 'IT Blogers',
+	description: 'IT Blogers for writing any posts and articles',
+}
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	return (
+		<html lang='en'>
+			<body className={montserrat.className}>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange
+					storageKey='blog-theme'
+				>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	)
+}
